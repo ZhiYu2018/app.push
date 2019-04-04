76b6a6772fac605dc3840cabcc79aa229d71f86f
知鱼 <39901140+ZhiYu2018@users.noreply.github.com>
2019-04-04 18:20:48 +0800

    Add files via upload

--- src/main/java/push/front/api/controller/Gateway.java
+++ src/main/java/push/front/api/controller/Gateway.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -38,6 +39,26 @@ public class Gateway {
 		rsp = router.router(req);
 		rsp.setSerialId(Util.randomString(18));
 		return rsp;
+	}
+	
+	@RequestMapping(value="/mgr", method = RequestMethod.POST)
+	@ResponseBody
+	public ApiResponse manage(@RequestParam(value = "name", required = true) String name,
+			                  @RequestParam(value = "pwd", required = true) String pwd){
+		ApiResponse resp = new ApiResponse();
+		String npwd = Util.getKey();
+		if(!name.equals("xmpp.go") || !pwd.equals(npwd)){
+			resp.setStat(404);
+			resp.setReason("Forbid");
+			logger.info("name {} != {}, pwd {} != {}", name, "xmpp.go", pwd, npwd);
+			return resp;
+			
+		}
+		
+		router.reconnect();
+		resp.setStat(200);
+		resp.setReason("OK");
+		return resp;
 	}
 
 }
